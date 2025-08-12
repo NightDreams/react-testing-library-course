f31df1c3335414f6f6e8229b6d010d33eb164d79
Angel Jair Neri Huerta <yayodcv@gmail.com>
2025-08-12 14:09:40 -0600

    test(login): add success case with mocks and navigation to /orders

--- src/containers/Login/Login.test.tsx
+++ src/containers/Login/Login.test.tsx
@@ -1,27 +1,38 @@
-import { act, fireEvent, render, screen } from '@testing-library/react'
+import { act, fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { Login } from './Login'
 import { MemoryRouter } from 'react-router-dom'
 import { SessionProvider } from '../../context/AuthContext'
 import { getAuth } from '../../services/getAuth'
 import { Mock } from 'vitest'
 
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom')
+  return {
+    ...actual,
+    useNavigate: () => mockNavigate,
+  }
+})
 vi.mock('../../services/getAuth', () => ({
   getAuth: vi.fn(),
 }))
 
+const mockNavigate = vi.fn()
 const mockGetAuth = getAuth as Mock
 
 describe('login', async () => {
-  it('login error ', async () => {
-    mockGetAuth.mockRejectedValueOnce(new Error('Invalid credentials'))
-
-    render(
+  const handleLogin = () => {
+    return render(
       <SessionProvider>
         <MemoryRouter>
           <Login />
         </MemoryRouter>
       </SessionProvider>
     )
+  }
+  it('login error ', async () => {
+    mockGetAuth.mockRejectedValue(new Error('Invalid credentials'))
+
+    handleLogin()
     // prepara ui elements for interaction
     const usernameInput = screen.getByPlaceholderText('Username')
     const passwordInput = screen.getByPlaceholderText('Password')
@@ -36,5 +47,23 @@ describe('login', async () => {
     // expects
     const errorMessage = screen.getByText('Invalid credentials')
     expect(errorMessage).toBeInTheDocument()
+  })
+
+  it('deberia redirigir a /orders', async () => {
+    mockGetAuth.mockResolvedValue({ success: true })
+    handleLogin()
+
+    const usernameInput = screen.getByPlaceholderText('Username')
+    const passwordInput = screen.getByPlaceholderText('Password')
+    const buttonLogin = screen.getByRole('button', { name: 'Login' })
+    await act(() => {
+      fireEvent.change(usernameInput, { target: { value: 'validUser' } })
+      fireEvent.change(passwordInput, { target: { value: 'validPassword' } })
+      fireEvent.click(buttonLogin)
+    })
+    await waitFor(() => {
+      expect(mockGetAuth).toHaveBeenCalledWith('validUser', 'validPassword')
+      expect(mockNavigate).toHaveBeenCalledWith('/orders')
+    })
   })
 })
