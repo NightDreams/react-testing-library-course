a507d04d1976e1159f0e79bf237ec0b1cd06301e
Angel Jair Neri Huerta <yayodcv@gmail.com>
2025-08-28 09:35:08 -0600

    chore(config): add path alias for imports in tsconfig and vite, and sass dependency to sass-embedded

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,30 @@
 /// <reference types="Vite/client" />
 
 import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // Puedes agregar opciones específicas de Sass aquí si lo necesitas
+        // silenceDeprecations: ['legacy-js-api'],
+      },
+    },
+  },
+  // experimental: {
+  //   sassApi: 'modern', // o 'modern-compiler'
+  // },
   test: {
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./src/setupTest.ts'],
+  },
+  resolve: {
+    alias: {
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+    },
   },
 })
