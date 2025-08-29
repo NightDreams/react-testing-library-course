7bfacaadc25ee4222121ca158dee9f2637b08a0e
Angel Jair Neri Huerta <yayodcv@gmail.com>
2025-08-29 15:12:16 -0600

    fix(tests): add alias for @hooks in vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -11,6 +12,11 @@ export default defineConfig({
     include: ['src/**/*.{test,spec}.{ts,tsx}'],
     coverage: {
       reporter: ['text', 'html'],
+    },
+  },
+  resolve: {
+    alias: {
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
     },
   },
 })
